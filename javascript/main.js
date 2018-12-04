9ad92b344162e5d39dcbe373169f67f0aa743009
Eugene <evgev465@gmail.com>
2018-12-04 18:19:05 +0200

    done

---
+++ javascript/main.js
@@ -0,0 +1,8 @@
+function collapsible(obj) {
+    obj.classList.toggle('collapsible-open');
+};
+
+function toggleNav() {
+    var el = document.getElementById('nav');
+    el.classList.toggle('open');
+}
